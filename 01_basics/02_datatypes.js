fab57d709cd68be28ec5327cc710c0ded9dcaffd
adeelmaaz77 <adeelmaaz40@gmail.com>
2025-02-04 09:40:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert ( 15 + 7) // we are using node.js, not bowser
+
+console.log(15 + 7)
+
+// code readability should be high
+
+console.log("Maaz")
+
+
+let name = "maaz"
+let age = 24
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
